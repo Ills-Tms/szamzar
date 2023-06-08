bae7f982de297f700cae27739fe3fc4efb1ce478
Ills-Tms <119455067+Ills-Tms@users.noreply.github.com>
2023-06-08 20:52:31 +0200

    js3

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const szam = document.querySelector('form');
-const jelszo=111
-let j1= document.querySelector('sz1');
-let j2= document.querySelector('sz2');
-let j3= document.querySelector('sz3');
-console.log(szam);
+const jelszo=111;
+let j1= document.getelementbyid('sz1');
+let j2= document.getelementbyid('sz2');
+let j3= document.getelementbyid('sz3');
+console.log(j1);
+console.log(j2);
+console.log(j3);
 
-if 
+
