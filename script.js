fe1e9d59c1f1aa671e663e43c211b3864cb6fe93
Ills-Tms <119455067+Ills-Tms@users.noreply.github.com>
2023-06-08 20:43:41 +0200

    js2

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const szam = document.querySelector('form');
 const jelszo=111
-
+let j1= document.querySelector('sz1');
+let j2= document.querySelector('sz2');
+let j3= document.querySelector('sz3');
 console.log(szam);
+
+if 
