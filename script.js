41c95785f1ee3ee4b776e2dc8ed603bf56813245
Ills-Tms <119455067+Ills-Tms@users.noreply.github.com>
2023-06-08 21:01:49 +0200

    js4

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const jelszo=111;
-let j1= document.getelementbyid('sz1');
-let j2= document.getelementbyid('sz2');
-let j3= document.getelementbyid('sz3');
-console.log(j1);
-console.log(j2);
-console.log(j3);
+const formelem = document.querySelector('#form')
+const egy = document.querySelector('sz1')
+const ketto = document.querySelector('sz2')
+const harom = document.querySelector('sz3')
 
+const jelszo = 111
 
+formelem.addEventListener('SUBMIT')
+if (egy + ketto + harom == jelszo) {
+  console.log('nyitva')
+} else {
+  console.log('zarva')
+}
